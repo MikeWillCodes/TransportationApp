57c074af9432e5ff09010152d9ea5c5b1dfaddda
Mike Williams <MikeWillCodes@gmail.com>
2018-04-14 19:05:31 -0500

    Initial commit

---
+++ src/com/company/HOV.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HOV {
+
+    private int cars;
+    private HOV next;
+
+    public HOV( int M){
+        cars = M;
+        next = null;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+
+    // Milestone 1
+    // TODO: 4/14/18 Create user defined LL for HOV lane
+    // TODO: 4/14/18 How many cars are in HOV Lane?
+    // TODO: 4/14/18 What are the id(s) of cars with only ONE occupant?
+    // TODO: 4/14/18 How many [C] colored cars are in the HOV lane?
+
+
+
+    public static void main(String[] args) {
+        System.out.println("========== Hello, User! ===========");
+
+        HOV hovLane = new HOV(10);
+        System.out.println("The HOV lane occupants" + hovLane);
+
+    }
+}
