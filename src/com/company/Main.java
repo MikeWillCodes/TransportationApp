b2b98870bc915094c1805a0c1506c5553e8cbee8
Mike Williams <MikeWillCodes@gmail.com>
2018-04-20 13:25:56 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,8 @@ public class Main {
 
         // Milestone 1 COMPLETE
         // 4/14/18 Create user defined LL for HOV lane
-        Random random = new Random();
-        int num = random.nextInt(10);
-        HOV lane = new HOV(num);
-        HOV lane2 = lane.split(num);
+        HOV lane = new HOV(20);
+        HOV lane2 = lane.split(12);
 
 
 
@@ -24,11 +22,14 @@ public class Main {
         System.out.println("HOV lane: " + lane);
 
         // 4/18/18 if HOV lane has [N] cars open up second HOV lane
-        System.out.println("HOV lane2: " + lane.split(4));
+        System.out.println("HOV lane2: " + lane2);
 
         // 4/14/18 What are the id(s) of cars with only ONE occupant?
         ArrayList<Integer> Ids = lane.isCarId(1);
-        System.out.println("\nID's of cars with [N] passengers: " + Ids);
+        System.out.println("\nID's of cars with [N] passengers 1: " + Ids);
+
+        ArrayList<Integer> Ids2 = lane2.isCarId(1);
+        System.out.println("ID's of cars with [N] passengers 2: " + Ids2);
 
         // How many [C] colored cars are in the HOV lane?
         ArrayList<String> colors = lane.isCarColor("white");
@@ -38,10 +39,8 @@ public class Main {
         System.out.println("# of cars with [C] color in lane 2: " + colors2.size());
 
         // TODO: 4/18/18 what was the most # of cars in lane [H] // Output total # of cars in each lane
-        
-        System.out.println("# of cars in lane 1 and lane 2: ");
 
-        // Milestone 2
+
         // TODO: 4/18/18 was it ever true HOV lan [A] and [B] had the same number of cars? BOOLEAN // if both ever had [N] cars
     }
 }
