52c02bd21fabcdcb7e78c00cc6b928c2a564e678
Mike Williams <MikeWillCodes@gmail.com>
2018-04-21 19:30:25 -0500

    update

--- src/com/company/CarRiser.java
+++ src/com/company/CarRiser.java
@@ -6,4 +6,15 @@ public class CarRiser {
     private Stack<Car> parking = new Stack<>();
     private int numOfCars;
     private int id;
+    private static int riserId = 1;
+
+    public CarRiser(){
+        this.id = riserId;
+        riserId++;
+    }
+    private void push(Car car){
+        numOfCars++;
+        parking.push(car);
+    }
+
 }
