1f966a3c7db1324a1eb9b8aa2751adfd9d2922bf
Mike Williams <MikeWillCodes@gmail.com>
2018-04-22 14:02:16 -0500

    update

--- src/com/company/carStack.java
+++ src/com/company/carStack.java
@@ -3,17 +3,21 @@ package com.company;
 import java.util.Stack;
 
 public class carStack {
+    // Stack the cars will be stored in. All methods will have access
+    // to the stack that's why its in the class not constructor
     private Stack<Car> garage = new Stack<>();
     // Number of cars in the garages
     private int cars;
-    // Every cars unique ID
-    private int carID;
-    // Garage number of each of (3) garages
+    // ID for garage
+    private int id;
+    // Garage number of each of (3) garages, static because I can call it
+    // without creating an object it belongs to
     private static int garageNum = 1;
 
 
     public carStack(){
-
+        this.id = garageNum;
+        garageNum++;
     }
 
     public int parkedCars(){
